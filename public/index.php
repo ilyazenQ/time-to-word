9d330291a7bd30ec45983db33ffc5f57327624b2
ilya <ilyazenq@gmail.com>
2023-02-28 11:31:59 +0800

    Task done

--- public/index.php
+++ public/index.php
@@ -21,62 +21,115 @@ class TimeToWordConverter implements TimeToWordConvertingInterface
         11 => 'одиннадцать',
         12 => 'двенадцать'
     ];
+
     public function convert(int $hours, int $minutes): string
     {
-        if(!$this->checkRangeTime($minutes,$hours)) {
+        if (!$this->checkRangeTime($minutes, $hours)) {
             throw new \InvalidArgumentException('Invalid request Time(out of range)');
         }
         if ($minutes == 0) {
-            return $this->responseWhenZeroMinutes($hours);
+            return $this->responseWhenZeroMinutes($hours) . '.';
         } elseif ($minutes == 1) {
-            return 'Одна минута после ' . $this->hoursToWords($hours).'.';
+            return 'Одна минута после ' . $this->hoursToWordsWhenBeforeAndAfter($hours) . '.';
         } elseif ($minutes == 59) {
-            return 'Одна минута до ' . $this->hoursToWords($hours+1).'.';
+            return 'Одна минута до ' . $this->hoursToWordsWhenBeforeAndAfter($hours + 1) . '.';
         } elseif ($minutes == 15) {
-            return 'Четверть ' . $this->hoursToWords($hours+1).'.';
+            return 'Четверть ' . $this->hoursToWordsWhenQuarterAndHalf($hours + 1) . '.';
         } elseif ($minutes == 30) {
-            return 'Половина ' . $this->hoursToWords($hours+1).'.';
+            return 'Половина ' . $this->hoursToWordsWhenQuarterAndHalf($hours + 1) . '.';
         } elseif ($minutes == 45) {
-            return 'Без пятнадцати минут ' . $this->hoursToWords($hours+1).'.';
+            return 'Без пятнадцати минут ' . $this->hoursToWords($hours + 1) . '.';
         } elseif ($minutes < 30) {
-            return $this->mb_strtoupper_first($this->minutesToWords($minutes)) . ' минут'. $this->getEndingToMinutes($minutes) .  ' после ' . $this->hoursToWords($hours).'.';
+            return mb_strtoupper_first($this->minutesToWords($minutes)) . ' минут' . $this->getEndingToMinutes($minutes) . ' после ' . $this->hoursToWordsWhenBeforeAndAfter($hours) . '.';
         } else {
             $minutesLeft = 60 - $minutes;
-            return $this->mb_strtoupper_first($this->minutesToWords($minutesLeft)) . ' минут'. $this->getEndingToMinutes($minutesLeft). ' до ' .$this->hoursToWords($hours+1).'.';
+            return mb_strtoupper_first($this->minutesToWords($minutesLeft)) . ' минут' . $this->getEndingToMinutes($minutesLeft) . ' до ' . $this->hoursToWordsWhenBeforeAndAfter($hours + 1) . '.';
         }
     }
 
-    private function getEndingToMinutes(int $minutes) {
-        if($minutes === 1) {
+    private function getEndingToMinutes(int $minutes): string
+    {
+        if ($minutes === 1) {
             return 'a';
         }
 
-        if($minutes > 10 && $minutes < 20) {
+        if ($minutes > 10 && $minutes < 20) {
             return '';
         }
 
-        $needEndingArray = [
-            '2','3','4'
+        $sameEndingArray = [
+            '2', '3', '4'
         ];
 
         $minutes = explode(' ', strval($minutes));
-        $lastMinutes = $minutes[count($minutes)-1];
-        if(in_array($lastMinutes, $needEndingArray)) {
+        $lastMinutes = $minutes[count($minutes) - 1];
+        if (in_array($lastMinutes, $sameEndingArray)) {
             return 'ы';
         } else {
             return '';
         }
     }
+
+    private function hoursToWordsWhenBeforeAndAfter(int $hours): string
+    {
+        if (!$this->checkHours($hours)) {
+            throw new \InvalidArgumentException('Invalid response Time(out of range)');
+        }
+        $units = [
+            '1' => 'одного',
+            '2' => 'двух',
+            '3' => 'трех',
+            '4' => 'четырёх',
+            '8' => 'восьми',
+        ];
+        $sameEndingArray = [
+            '5', '6', '7', '9', '10', '11', '12'
+        ];
+
+        if (in_array(strval($hours), $sameEndingArray)) {
+            return substr_replace($this->hourWords[$hours], 'и', -2);
+        }
+
+        return $units[$hours];
+    }
+
+    private function hoursToWordsWhenQuarterAndHalf(int $hours): string
+    {
+        if (!$this->checkHours($hours)) {
+            throw new \InvalidArgumentException('Invalid response Time(out of range)');
+        }
+
+        $units = [
+            '1' => 'первого',
+            '2' => 'второго',
+            '3' => 'третьего',
+            '4' => 'четвёртого',
+            '8' => 'восьмого',
+        ];
+
+        $sameEndingArray = [
+            '5', '6', '7', '9', '10', '11', '12'
+        ];
+
+        if (in_array(strval($hours), $sameEndingArray)) {
+            return substr_replace($this->hourWords[$hours], 'ого', -2);
+        }
+
+        return $units[$hours];
+    }
+
+
     private function hoursToWords(int $hours): string
     {
-        if(!$this->checkHours($hours)) {
+        if (!$this->checkHours($hours)) {
             throw new \InvalidArgumentException('Invalid response Time(out of range)');
         }
         return $this->hourWords[$hours];
     }
+
     private function checkMinutes(int $minutes): bool
     {
-        if($minutes < 0 || $minutes > 60) {
+        if ($minutes < 0 || $minutes > 60) {
             return false;
         }
         return true;
@@ -84,25 +137,27 @@ class TimeToWordConverter implements TimeToWordConvertingInterface
 
     private function checkHours(int $hours): bool
     {
-        if($hours < 0 || $hours > 12) {
-            return false;
-        }
-        return true;
-    }
-    private function checkRangeTime(int $minutes,int $hours):bool
-    {
-        if(!$this->checkMinutes($minutes) || !$this->checkHours($hours)) {
+        if ($hours < 0 || $hours > 12) {
             return false;
         }
         return true;
     }
 
-    private function responseWhenZeroMinutes(int $hours): string {
+    private function checkRangeTime(int $minutes, int $hours): bool
+    {
+        if (!$this->checkMinutes($minutes) || !$this->checkHours($hours)) {
+            return false;
+        }
+        return true;
+    }
+
+    private function responseWhenZeroMinutes(int $hours): string
+    {
         $post = ' часов';
-        if($hours === 1) {
+        if ($hours === 1) {
             $post = ' час';
         }
-        return $this->mb_strtoupper_first($this->hourWords[$hours]) . $post;
+        return mb_strtoupper_first($this->hourWords[$hours]) . $post;
     }
 
     private function minutesToWords(int $minutes): string
@@ -133,8 +188,7 @@ class TimeToWordConverter implements TimeToWordConvertingInterface
             50 => 'пятьдесят'
         ];
 
-        if ($minutes <= 20)
-        {
+        if ($minutes <= 20) {
             return $units[$minutes];
         } elseif ($minutes < 30) {
             return $units[20] . ' ' . $units[$minutes - 20];
@@ -152,13 +206,15 @@ class TimeToWordConverter implements TimeToWordConvertingInterface
             return $units[50] . ' ' . $units[$minutes - 50];
         }
     }
-    function mb_strtoupper_first($str, $encoding = 'UTF8')
-    {
-        return
-            mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding) .
-            mb_substr($str, 1, mb_strlen($str, $encoding), $encoding);
-    }
 }
+
+function mb_strtoupper_first(string $str, string $encoding = 'UTF8'): string
+{
+    return
+        mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding) .
+        mb_substr($str, 1, mb_strlen($str, $encoding), $encoding);
+}
+
 $converter = new TimeToWordConverter();
 
 echo $converter->convert(7, 0) . "<br>"; // Семь часов
@@ -169,6 +225,5 @@ echo $converter->convert(7, 15) . "<br>"; // Четверть восьмого
 echo $converter->convert(7, 22) . "<br>"; // Двадцать две минуты после семи
 echo $converter->convert(7, 30) . "<br>"; // Половина восьмого
 echo $converter->convert(7, 35) . "<br>"; // Двадцать пять минут до восьми
-echo $converter->convert(7, 41) . "<br>"; // Девятнадцать минут до восьми
+echo $converter->convert(7, 45) . "<br>"; // Без пятнадцати минут восемь
 echo $converter->convert(7, 56) . "<br>"; // Четыре минуты до восьми
-echo $converter->convert(10, 59) . "<br>"; // Одна минута до восьми
